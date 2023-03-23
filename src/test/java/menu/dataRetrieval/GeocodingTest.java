4358c42ff89beffc49986047503e23c1aafee150
Maciej Anulewicz <hj2aaf@gmail.com>
2023-03-23 16:47:59 +0100

    Created Geocoding.java (To check coordinates based on entered city name by user) and GeocodingTest.java

---
+++ src/test/java/menu/dataRetrieval/GeocodingTest.java
@@ -0,0 +1,22 @@
+package menu.dataRetrieval;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeocodingTest {
+
+    @Test
+    void importCoordinatesData01() {
+        String[] input = new String[] {"London", "England"};
+        double[] test01 = new double[] {51.5073219, -0.1276474};
+        assertArrayEquals(test01, Geocoding.importCoordinatesData(input));
+    }
+
+    @Test
+    void importCoordinatesData02() {
+        String[] input = new String[] {"Warsaw", ""};
+        double[] test02 = new double[] {52.2319581, 21.0067249};
+        assertArrayEquals(test02, Geocoding.importCoordinatesData(input));
+    }
+}
